077dcefb76a4b7930179ce378c490b2693894ca1
Kostyasan1214 <166634803+Kostyasan1214@users.noreply.github.com>
2024-05-17 20:45:01 +0300

    Update Тест123.cpp
    Переделал задание

--- Тест123.cpp
+++ Тест123.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -10,17 +10,9 @@ std::vector<int> Vec(std::vector<int> vec, int n) {
     return vec;
 }
 
-bool testVec() {
-    std::vector<int> vec1 = { 1, 2, 3, 4, 5 };
-    std::vector<int> result1 = Vec(vec1, 10);
-    if (vec1[1] + 10 == result1[1] && vec1[2] + 10 == result1[2] && vec1[3] + 10 == result1[3]) {
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
+TEST(Vec, PositiveNumbers) {
+    std::vector<int> input = {1, 2, 3, 4, 5};
+    std::vector<int> expected = {4, 5, 6, 7, 8};
 
-int main() {
-    cout << testVec();
+    ASSERT_EQ(Vec(input, 3), expected);
 }
